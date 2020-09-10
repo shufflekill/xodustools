244fc2aa90d76fddb36fb7215f6a8b76ba8bf697
Shuffle <40589838+shufflekill@users.noreply.github.com>
2020-09-10 11:41:34 +1000

    Update installimunify.sh

--- installimunify.sh
+++ installimunify.sh
@@ -3,6 +3,8 @@
 echo "removing CXS from system"
 sh /etc/cxs/uninstall.sh
 
+echo "Grabbing Files"
+wget https://raw.githubusercontent.com/shufflekill/xodustools/master/imunify360.config
 wget https://repo.imunify360.cloudlinux.com/defence360/i360deploy.sh
 bash i360deploy.sh
 rm -f i360deploy.sh
@@ -10,8 +12,13 @@ rm -f i360deploy.sh
 echo "Installing KernelCare..."
 curl -s https://repo.cloudlinux.com/kernelcare/kernelcare_install.sh | bash
 
-
+echo "Registering Agent (Sometimes Needed)"
 imunify360-agent register IPL
+
+echo "Removing Standard Config"
+rm /etc/sysconfig/imunify360/imunify360.config
+echo "Copying SAU Config"
+mv imunify360.config /etc/sysconfig/imunify360/imunify360.config
 
 echo "Adding SAU Nagios IP To Whitelist"
 imunify360-agent whitelist ip add 202.130.35.150 || imunify360-agent whitelist ip move 202.130.35.150    # Nagios
